a81a7cbf854bceddb8c095f6056b0dc891007f60
yuntingc <chiayunting@gmail.com>
2022-12-10 00:05:03 +0800

    added auth

--- routers/messagesRouter.js
+++ routers/messagesRouter.js
@@ -2,22 +2,30 @@ const express = require("express");
 const router = express.Router();
 
 class MessagesRouter {
-  constructor(controller) {
+  constructor(controller, auth) {
     this.controller = controller;
+    this.auth = auth;
   }
   routes() {
     // this is the page upon clicking a particular chat
     router.get(
       "/:chatId",
+      this.auth,
       this.controller.getAllMessages.bind(this.controller)
     );
-    router.post("/", this.controller.postNewMessage.bind(this.controller));
+    router.post(
+      "/",
+      this.auth,
+      this.controller.postNewMessage.bind(this.controller)
+    );
     router.put(
       "/:messageId",
+      this.auth,
       this.controller.editMessage.bind(this.controller)
     );
     router.delete(
       "/:messageId",
+      this.auth,
       this.controller.deleteMessage.bind(this.controller)
     );
     return router;
